0166f08683eda43109d1db1ec9ddab463474333c
andrew <sanakurin@gmail.com>
2023-02-26 13:35:33 +0300

    added models; made migrations

--- js_dj_proj/employee_list/admin.py
+++ js_dj_proj/employee_list/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from employee_list.models import *
+
+@admin.register(Department)
+class DepAdmin(admin.ModelAdmin):
+    list_display = ["dep_name"]
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ["tit_name"]
+
+@admin.register(Employee)
+class EmpAdmin(admin.ModelAdmin):
+    list_display = ["second_name", "name", "last_name"]
--- js_dj_proj/employee_list/models.py
+++ js_dj_proj/employee_list/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    dep_name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.dep_name
+
+class Title(models.Model):
+    tit_name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.tit_name
+
+class Employee(models.Model):
+    name = models.CharField(max_length=40)
+    second_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+
+    emp_department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    emp_title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.second_name + ' ' + self.second_name + ' ' + self.last_name}"

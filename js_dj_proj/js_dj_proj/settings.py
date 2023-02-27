ed73462687659056ebcf46bea4e9f63b8d8f2cf8
andrew <sanakurin@gmail.com>
2023-02-27 18:26:20 +0300

    changed to localhost

--- js_dj_proj/js_dj_proj/settings.py
+++ js_dj_proj/js_dj_proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tqzm_z^$n#jdyc!@1jt4j%niwxtr=1wf*x^0=9_m^#-th^7ax0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.12']
+ALLOWED_HOSTS = ['192.168.0.12', '127.0.0.1']
 
 
 # Application definition
